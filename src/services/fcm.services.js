a9f69f03b87c3356507f05f586818046ec41afcc
Owen <77976026+Owen7000@users.noreply.github.com>
2026-02-19 11:58:40 +0000

    Changes to FCM service
    Now the service account keys are stored in environment variables, meaning that I can set them up on vercel properly.
    
    There is no longer a need to have a json file with the keys in it

--- src/services/fcm.services.js
+++ src/services/fcm.services.js
@@ -1,6 +1,10 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../../serviceAccountKey.json");
+const serviceAccount = JSON.parse(
+    process.env.FIREBASE_SERVICE_ACCOUNT
+);
+
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, "\n");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
