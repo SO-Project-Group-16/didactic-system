ddb305d848584a3c7d6a37e3ca5cd894c4727407
Owen <77976026+Owen7000@users.noreply.github.com>
2026-02-20 11:54:39 +0000

    Update login.controller.js
    Allow users to log in to the api.
    This has been tested locally, so should work once redeployed on vercel.
    
    It's worth noting that signing the JWT with 'secret' is not the long term plan.

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,3 +1,7 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const prisma = require("../utils/prisma");
+
 /**
  * Validate the incoming POST data to check if the user is allowed to log in
  */
@@ -10,9 +14,46 @@ const attemptLogin = async (req, res) => {
         });
     }
 
-    res.status(200).json({
-        userApiKey: "API_KEY_HERE",
+    // Check for a user with the given email address
+    const userMatch = await prisma.user.findUnique({
+        where: { emailAddress: username }
     })
+
+    if (userMatch != null){        
+        // Check the password
+        bcrypt.compare(password, userMatch.pwHash, function(err, result) {
+            if (result) {
+                console.log("Password hash is valid");
+
+                // Generate the access token for the api
+                const token = jwt.sign({ username: username }, 'secret', {
+                    expiresIn: '1h',
+                });
+
+                // console.log(jwt.verify(token, 'secret').username);
+
+                res.status(200).json({
+                    userApiKey: token,
+                })
+            } else {
+                console.log("Invalid password");
+                
+                res.status(400).json({
+                    error: "Invalid Credentials",
+                })
+            }
+        });
+    } else {
+        // For data security reasons, if no user account exists, the error
+        // is still Invalid Credentials. I want to ensure that you can't brute force 
+        // your way into stealing credentials.
+
+        console.log("No user account found")
+
+        res.status(400).json({
+            error: "Invalid Credentials",
+        })
+    }
 }
 
 module.exports = {
