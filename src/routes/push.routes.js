4e1edf4ac1a6ae638c2a1d89a5cb4890b71934a5
jp2063 <jp2063@hw.ac.uk>
2026-03-08 23:23:34 +0000

    added getThresholds controller, needs tested

--- src/routes/push.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { registerToken, unregisterDevice } = require("../controllers/push.controller");
-
-router.post("/register", registerToken);
-router.post("/unregister", unregisterDevice);
-
-module.exports = router;
