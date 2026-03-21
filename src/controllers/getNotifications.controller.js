cbdd2f10e795225b32634c864b14cad10dd1e6f7
Owen <77976026+Owen7000@users.noreply.github.com>
2026-03-21 11:09:50 +0000

    Make notifications nicer
    Added extra formatting options for dates that are less than a week old, and added appointment information to appointment notifications

--- src/controllers/getNotifications.controller.js
+++ src/controllers/getNotifications.controller.js
@@ -1,17 +1,50 @@
 const jwt = require("jsonwebtoken");
 const prisma = require("../utils/prisma");
+const { app } = require("firebase-admin");
 
+const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+/**
+ * Check if a given date object is less than a week old
+ * 
+ * @param {Date} date 
+ * @returns True if the date is less than a week old, false otherwise
+ */
+function isDateLessThanWeekOld(date) {
+    return date > Date.now() - (1000 * 60 * 60 * 24 * 7);
+}
+
+/**
+ * Format a given date object into a human readable format. 
+ * If the date is less than a week old, it will come out with the short name of the day at the begining,
+ * otherwise it will come out in the dd/mm/yyyy format
+ * 
+ * @param {Date} date 
+ * @returns A string containing the date in a human readable format
+ */
 function formatDate(date) {
     const d = new Date(date);
 
-    const day = String(d.getDate()).padStart(2, "0");
-    const month = String(d.getMonth() + 1).padStart(2, "0");
-    const year = d.getFullYear();
+    if (isDateLessThanWeekOld(d)) {
+        // Date is less than a week old, format the date as the day of the week
 
-    const hours = String(d.getHours()).padStart(2,"0)");
-    const minutes = String(d.getMinutes()).padStart(2, "0");
+        const day = dayNames[d.getDay()];
+        const hours = String(d.getHours()).padStart(2,"0)");
+        const minutes = String(d.getMinutes()).padStart(2, "0");
 
-    return `${day}/${month}/${year} ${hours}:${minutes}`;
+        return `${day} ${hours}:${minutes}`;
+    } else {
+        // Date is more than a week old, use the original date formatting method
+
+        const day = String(d.getDate()).padStart(2, "0");
+        const month = String(d.getMonth() + 1).padStart(2, "0");
+        const year = d.getFullYear();
+
+        const hours = String(d.getHours()).padStart(2,"0)");
+        const minutes = String(d.getMinutes()).padStart(2, "0");
+
+        return `${day}/${month}/${year} ${hours}:${minutes}`;
+    }
 }
 
 const getNotifications = async (req, res) => {
@@ -43,8 +76,24 @@ const getNotifications = async (req, res) => {
                 sent: formatDate(notification.sent),
                 content: notification.content
             };
+<<<<<<< Updated upstream
             if (notification.appoitmentId !== null) {
+=======
+
+            if (isAppointment) {
+                // This makes appointments include details in the notification so the user knows what to do/where to go
+>>>>>>> Stashed changes
                 item["appointment-id"] = notification.appoitmentId;
+
+                // Get the appointment from the db
+                const appointment = await prisma.appointment.findFirst({
+                    where: {
+                        appointmentId: notification.appointmentId
+                    }
+                });
+
+
+                item.content = `You have an appointment with your clinician on ${appointment.datetime}. The appointment will be at ${appointment.location} and is regarding: ${appointment.purpose}`;
             }
 
             result[notification.notificationId] = item;
